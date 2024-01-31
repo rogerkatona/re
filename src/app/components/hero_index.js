4270702428e9624bc6d9b7b3273e5d04457695ae
Roger Katona <roger.katona@gmail.com>
2024-01-31 09:03:34 -0800

    Added video background to website!

--- src/app/components/hero_index.js
+++ src/app/components/hero_index.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Modal from "@/app/components/modal";
 import {useState} from "react";
 import useModal from "@/app/lib/useModal";
+import VideoBackground from "@/app/components/video-background";
 
 export default function HeroIndex({ children, home }) {
 
@@ -13,26 +14,28 @@ export default function HeroIndex({ children, home }) {
 
     return (
         <>
-        <section className="bg-hero-index bg-cover md:bg-fixed bg-no-repeat md:bg-center">
-            <div className="flex lg:flex-row flex-col-reverse justify-center lg:items-center lg:mx-auto lg:max-w-8xl h-screen90vh md:px-0">
-                <div className="text-center">
-                    <div>
-                        <div className="whitespace-break-spaces font-raleway font-bold lg:text-7xl text-5xl text-platinum pb-4">
-                            <p>Where Creativity Meets Conversion:</p>
-                            <p>Unleash the Power of Your Brand.</p>
+        <section className="">
+
+                <VideoBackground />
+                <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">
+                    <div className="text-center">
+                        <div>
+                            <div className="whitespace-break-spaces font-raleway font-bold lg:text-7xl text-5xl text-platinum pb-4">
+                                <p>Where Creativity Meets Conversion:</p>
+                                <p>Unleash the Power of Your Brand.</p>
+                            </div>
+                        </div>
+                        <div>
+                            <Link href=''>
+                                <button
+                                    onClick={toggle}
+                                    className="bg-vanilla-200 hover:bg-vanilla-100 text-xs text-platinum hover:text-platinum-300 uppercase px-4 py-3 rounded-lg md:ml-3 md:mt-0 mt-6">
+                                    Contact us
+                                </button>
+                            </Link>
                         </div>
                     </div>
-                    <div>
-                        <Link href=''>
-                            <button
-                                onClick={toggle}
-                                className="bg-vanilla-200 hover:bg-vanilla-100 text-xs text-platinum hover:text-platinum-300 uppercase px-4 py-3 rounded-lg md:ml-3 md:mt-0 mt-6">
-                                Contact us
-                            </button>
-                        </Link>
-                    </div>
                 </div>
-            </div>
         </section>
         <section>
             <Modal
---
+++ src/app/components/video-background.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const VideoBackground = () => {
+    return (
+        <div className="relative h-screen bg-rich_black-500">
+            <video
+                autoPlay
+                muted
+                loop
+                className="object-cover object-center w-full h-full opacity-10 "
+            >
+                <source src="/video/AdobeStock_574452973_Video_4K_Preview.mov" type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
+        </div>
+    );
+};
+
+export default VideoBackground;
